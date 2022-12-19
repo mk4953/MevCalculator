ff95a2678b9bf9fc0920ca68d4682143b3becb30
Mev <mhk_4953@live.dk>
2022-12-19 09:19:36 +0100

    Exponential Calculation has been added

---
+++ Math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Powered(2,5);
+        }
+
+        public static long Powered(long number, long exponent )
+        {
+            var temp = number;
+            for (int i = 0; i < exponent-1; i++)
+            {
+                number = number * temp;
+            }
+            
+            return number;
+        }
+    }
+}
